c8835bbe5af7da8b8cbbc6dfe5125bbc0743b7e4
nicholasferretti <nferretti@gyroapp.co.za>
2024-05-03 14:19:27 +0200

    Created FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Nicholas Ferretti on 2024/05/03.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
