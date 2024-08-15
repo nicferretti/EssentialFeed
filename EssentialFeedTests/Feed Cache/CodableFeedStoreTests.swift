a2494d8e6c6db1a0375d1b9296278b35aada5b8f
nicferretti <ferrettinicholas@gmail.com>
2024-08-15 23:03:15 +0200

    Extract `FailableDeleteFeedStoreSpecs` checks into reusable assertions

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -124,19 +124,14 @@ final class CodableFeedStoreTests: XCTestCase, FailableFeedStore {
         let noDeletePermissionURL = cachesDirectory()
         let sut = makeSUT(storeURL: noDeletePermissionURL)
 
-        let deletionError = deleteCache(from: sut)
-
-        XCTAssertNotNil(deletionError, "Expcted cache deletion to fail")
-        expect(sut, toRetrieve: .empty)
+        assertThatDeleteDeliversErrorOnDeletionError(on: sut)
     }
 
     func test_delete_hasNoSideEffectsOnDeletionError() {
         let noDeletePermissionURL = cachesDirectory()
         let sut = makeSUT(storeURL: noDeletePermissionURL)
 
-        deleteCache(from: sut)
-
-        expect(sut, toRetrieve: .empty)
+        assertThatDeleteHasNoSideEffectsOnDeletionError(on: sut)
     }
 
     func test_storeSideEffects_runSerially() {
---
+++ EssentialFeedTests/Feed Cache/XCTestCase+FailableDeleteFeedStoreSpecs.swift
@@ -0,0 +1,23 @@
+//
+//  XCTestCase+FailableDeleteFeedStoreSpecs.swift
+//  EssentialFeedTests
+//
+//  Created by Nicholas Ferretti on 2024/08/15.
+//
+
+import XCTest
+import EssentialFeed
+
+extension FailableDeleteFeedStoreSpecs where Self: XCTestCase {
+    func assertThatDeleteDeliversErrorOnDeletionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+        let deletionError = deleteCache(from: sut)
+
+        XCTAssertNotNil(deletionError, "Expcted cache deletion to fail")
+    }
+
+    func assertThatDeleteHasNoSideEffectsOnDeletionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+        deleteCache(from: sut)
+
+        expect(sut, toRetrieve: .empty)
+    }
+}
