4f38858cb8761ec5a25016eb8d7191695b120c11
nicferretti <ferrettinicholas@gmail.com>
2024-10-30 16:22:03 +0200

    Move `FeedImagePresenter`, `FeedImageViewModel` and collaborators to production

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedImageViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Nicholas Ferretti on 2024/09/30.
+//
+
+public struct FeedImageViewModel<Image> {
+    public let description: String?
+    public let location: String?
+    public let image: Image?
+    public let isLoading: Bool
+    public let shouldRetry: Bool
+    public var hasLocation: Bool {
+        return location != nil
+    }
+}
