f85e35e0bbdad0c56aaacbda03bafd95a9fb9b3e
nicferretti <ferrettinicholas@gmail.com>
2024-12-12 23:29:37 +0200

    Display error on mapper error

--- EssentialFeed/EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
+++ EssentialFeed/EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
@@ -12,7 +12,7 @@ public protocol ResourceView {
 }
 
 public class LoadResourcePresenter<Resource, View: ResourceView> {
-    public typealias Mapper = (Resource) -> View.ResourceViewModel
+    public typealias Mapper = (Resource) throws -> View.ResourceViewModel
 
     private let resourceView: View
     private let loadingView: ResourceLoadingView
@@ -39,8 +39,12 @@ public class LoadResourcePresenter<Resource, View: ResourceView> {
     }
 
     public func didFinishLoading(with resource: Resource) {
-        resourceView.display(mapper(resource))
-        loadingView.display(ResourceLoadingViewModel(isLoading: false))
+        do {
+            resourceView.display(try mapper(resource))
+            loadingView.display(ResourceLoadingViewModel(isLoading: false))
+        } catch {
+            didFinishLoading(with: error)
+        }
     }
 
     public func didFinishLoading(with error: Error) {
