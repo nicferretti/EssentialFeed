38302724ef3840045b93d074dd29234573b54e88
nicferretti <ferrettinicholas@gmail.com>
2024-12-12 16:38:57 +0200

    Replace "FEED_VIEW_CONNECTION_ERROR" with "GENERIC_CONNECTION_ERROR"

--- EssentialFeed/EssentialFeedTests/Shared Presentation/LoadResourcePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Shared Presentation/LoadResourcePresenterTests.swift
@@ -40,13 +40,13 @@ final class LoadResourcePresenterTests: XCTestCase {
         ])
     }
 
-    func test_didFinishLoadingFeedWithError_displaysLocalizedErrorAndStopsLoading() {
+    func test_didFinishLoadingWithError_displaysLocalizedErrorAndStopsLoading() {
         let (sut, view) = makeSUT()
 
-        sut.didFinishLoadingFeed(with: anyNSError)
+        sut.didFinishLoading(with: anyNSError)
 
         XCTAssertEqual(view.messages, [
-            .display(errorMessage: localized("FEED_VIEW_CONNECTION_ERROR")),
+            .display(errorMessage: localized("GENERIC_CONNECTION_ERROR")),
             .display(isLoading: false)
         ])
     }
