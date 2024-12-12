aca52a693e33a6d07f29ae570ddc54678d4aaae5
nicferretti <ferrettinicholas@gmail.com>
2024-12-12 17:04:51 +0200

    Move "GENERIC_CONNECTION_ERROR" localization key to new Shared strings

--- EssentialApp/EssentialAppTests/Helpers/FeedViewControllerTests+Localization.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedViewControllerTests+Localization.swift
@@ -10,15 +10,17 @@ import XCTest
 import EssentialFeed
 
 extension FeedUIIntegrationTests {
-    func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
-        let table = "Feed"
-        let bundle = Bundle(for: FeedPresenter.self)
-        let value = bundle.localizedString(forKey: key, value: nil, table: table)
+    private class DummyView: ResourceView {
+        func display(_ viewModel: Any) {
 
-        if value == key {
-            XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
         }
+    }
+    
+    var loadError: String {
+        LoadResourcePresenter<Any, DummyView>.loadError
+    }
 
-        return value
+    var feedTitle: String {
+        FeedPresenter.title
     }
 }
