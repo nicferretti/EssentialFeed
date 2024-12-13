ddd26436be97f4421ebe3ed65697be70cafc9bb2
nicferretti <ferrettinicholas@gmail.com>
2024-12-13 20:06:08 +0200

    Add image comments title and localize comments title

---
+++ EssentialFeed/EssentialFeed/Image Comments Presentation/ImageCommentsPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCommentsPresenter.swift
+//  EssentialFeed
+//
+//  Created by Nicholas Ferretti on 2024/12/13.
+//
+
+public final class ImageCommentsPresenter {
+
+    public static var title: String {
+        NSLocalizedString("IMAGE_COMMENTS_VIEW_TITLE", tableName: "ImageComments", bundle: Bundle(for: Self.self), comment: "Title for the image comments view")
+    }
+}
---
+++ EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentsLocalizationTests.swift
@@ -0,0 +1,20 @@
+//
+//  ImageCommentsLocalizationTests.swift
+//  EssentialFeed
+//
+//  Created by Nicholas Ferretti on 2024/12/13.
+//
+
+import XCTest
+import EssentialFeed
+
+final class ImageCommentsLocalizationTests: XCTestCase {
+
+    func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+        let table = "ImageComments"
+        let bundle = Bundle(for: ImageCommentsPresenter.self)
+        
+        assertLocalizedKeyAndValuesExist(in: bundle, table)
+    }
+
+}
