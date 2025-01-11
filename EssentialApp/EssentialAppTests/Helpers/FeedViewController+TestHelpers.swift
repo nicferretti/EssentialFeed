ef203956bdafe69a2a290a08887b8102da872866
nicferretti <ferrettinicholas@gmail.com>
2025-01-11 16:00:44 +0200

    Load actions requests comments

--- EssentialApp/EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
@@ -42,7 +42,7 @@ extension ListViewController {
         tableView.frame = CGRect(x: 0, y: 0, width: 390, height: 1)
     }
 
-    func simulateUserInitiatedFeedReload() {
+    func simulateUserInitiatedReload() {
         refreshControl?.simulatePullToRefresh()
     }
 
